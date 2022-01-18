6e9c56d36b8e3a6d8d344c33e49f29dfc01f0110
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-01-18 20:20:31 +0300

    start with desktop, add css reset rules to app.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div class="mainComponent"></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+export default {};
 </script>
+
+<style scoped>
+.mainComponent {
+  height: 100%;
+  width: 100%;
+  background: #e5e5e5;
+}
+</style>
