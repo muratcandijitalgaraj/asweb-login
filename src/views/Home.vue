ffb1e0a5a04169ee0bfaf50168e67aa3585f015e
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-01-18 20:35:13 +0300

    create the skeleton, add bulutlar background

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="mainComponent"></div>
+  <div class="mainComponent">
+    <Desktop />
+  </div>
 </template>
 
 <script>
-export default {};
+import Desktop from "./giris/desktop/Desktop.vue";
+export default { components: { Desktop } };
 </script>
 
 <style scoped>
 .mainComponent {
+  /* height: 100%;
+  width: 100%;
+  background: #e5e5e5; */
   height: 100%;
   width: 100%;
-  background: #e5e5e5;
 }
 </style>
---
+++ src/views/giris/desktop/Desktop.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="desktopComponent">
+    <Bulutlar />
+  </div>
+</template>
+
+<script>
+import Bulutlar from "./Bulutlar.vue";
+export default { components: { Bulutlar } };
+</script>
+
+<style scoped>
+.desktopComponent {
+  height: 100%;
+  width: 100%;
+  background: #e5e5e5;
+}
+</style>
